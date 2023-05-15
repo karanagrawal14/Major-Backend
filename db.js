47d3c6a8a0252c372290ec72536360a30e8617a4
karanagrawal14 <karanagrawal1411@gmail.com>
2023-05-15 17:34:26 +0530

    2.0

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 module.exports = {
     // database: 'mongodb://localhost:27017/bookdb'
-    database:'mongodb+srv://karanagrawal:JPxCuH85uKQyvh2o@cluster0.bajquhv.mongodb.net/mern?retryWrites=true&w=majority'
+    // database:'mongodb+srv://karanagrawal:JPxCuH85uKQyvh2o@cluster0.bajquhv.mongodb.net/mern?retryWrites=true&w=majority'
+    database:'mongodb+srv://shubham:08082002@cluster0.s1m1n7g.mongodb.net/'
 };
 
 //pass JPxCuH85uKQyvh2o
